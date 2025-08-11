9549402629f085779bfc7b310f7bc2995b25c1bc
이지민 <147581729+JiMinL03@users.noreply.github.com>
2025-08-11 14:55:16 +0900

    Title: 달에서 무게 구하기, Time: 98ms, Memory: 9MB, Status: Passed - Codetree

--- 250811/달에서 무게 구하기/weight-on-the-moon.java
+++ 250811/달에서 무게 구하기/weight-on-the-moon.java
@@ -4,6 +4,6 @@ public class Main {
         int a; double b;
         a=13; b=0.165;
         double sum = a*b;
-        System.out.print("%.6d", sum);
+        System.out.printf("%d * %f = %.6f",a,b, sum);
     }
 }
