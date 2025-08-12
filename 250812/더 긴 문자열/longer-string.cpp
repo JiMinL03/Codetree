824f188c016c8326d1f235342e34bd2d2376741f
이지민 <147581729+JiMinL03@users.noreply.github.com>
2025-08-12 14:45:57 +0900

    Title: 더 긴 문자열, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250812/더 긴 문자열/longer-string.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+using namespace std;
+
+int main() {
+    string str1, str2;   
+    cin >> str1 >> str2;
+    int len1 = strlen(str1);
+    int len2 = strlen(str2);
+
+    if(len1 > len) cout << str1 << " " << len1;
+    else cout << str2 << " " << len2;
+    return 0;
+}
