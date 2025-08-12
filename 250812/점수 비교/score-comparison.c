702885f7aa603eb9cd888b4ee89b05965847308f
이지민 <147581729+JiMinL03@users.noreply.github.com>
2025-08-12 11:19:33 +0900

    Title: 점수 비교, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250812/점수 비교/score-comparison.c
+++ 250812/점수 비교/score-comparison.c
@@ -4,10 +4,10 @@ int main() {
     // Please write your code here.
     int math1, math2;
     int eng1, eng2;
-    scanf("%d %d", &math1, &math2);
-    scanf("%d %d", &eng1, &eng2);
+    scanf("%d %d", &math1, &eng1);
+    scanf("%d %d", &math2, &eng2);
 
-    if(math1 >= math2 && eng1 >= eng2) printf("1");
+    if(math1 > math2 && eng1 > eng2) printf("1");
     else printf("0");
     return 0;
 }
