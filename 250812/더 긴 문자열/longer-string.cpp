db66277e371f3dda67b39ab2b53545c0a8cacbe6
이지민 <147581729+JiMinL03@users.noreply.github.com>
2025-08-12 14:57:23 +0900

    Title: 더 긴 문자열, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 250812/더 긴 문자열/longer-string.cpp
+++ 250812/더 긴 문자열/longer-string.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 #include <string>
-#include <cstring>
 using namespace std;
 
 int main() {
-    string str1, str2;   
+    string str1, str2;
     cin >> str1 >> str2;
-    int len1 = strlen(str1);
-    int len2 = strlen(str2);
 
-    if(len1 > len) cout << str1 << " " << len1;
+    int len1 = str1.length();
+    int len2 = str2.length();
+
+    if (len1 > len2) cout << str1 << " " << len1;
     else cout << str2 << " " << len2;
     return 0;
 }
