2ac4c9e8b229331dbfdcdb2c4d4755192ca0ad68
이지민 <147581729+JiMinL03@users.noreply.github.com>
2025-08-12 11:15:24 +0900

    Title: 점수 비교, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250812/점수 비교/score-comparison.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int math1, math2;
+    int eng1, eng2;
+    scanf("%d %d", &math1, &math2);
+    scanf("%d %d", &eng1, &eng2);
+
+    if(math1 > math2 && eng1 > eng2) printf("1");
+    else printf("0");
+    return 0;
+}
