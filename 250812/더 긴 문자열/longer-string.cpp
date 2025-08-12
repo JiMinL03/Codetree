d10e5141857c57e9f59304633fab98c3b9e3cb8c
이지민 <147581729+JiMinL03@users.noreply.github.com>
2025-08-12 15:00:35 +0900

    Title: 더 긴 문자열, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250812/더 긴 문자열/longer-string.cpp
+++ 250812/더 긴 문자열/longer-string.cpp
@@ -9,7 +9,8 @@ int main() {
     int len1 = str1.length();
     int len2 = str2.length();
 
-    if (len1 > len2) cout << str1 << " " << len1;
+    if (len1 == len2) cout<< "same";
+    else if (len1 > len2) cout << str1 << " " << len1;
     else cout << str2 << " " << len2;
     return 0;
 }
